05706008c3bab38f2eb6e12fb9f97a21ba3e8ae3
mahdesignstudio <mahdesignstudio@gmail.com>
2020-03-23 18:39:14 -0400

    add require_relative file path for lib files

--- lib/cosmetics_cli.rb
+++ lib/cosmetics_cli.rb
@@ -1,5 +1,9 @@
 require_relative './cosmetics_cli/version.rb'
 
+require_relative './cosmetics_cli/api'
+require_relative './cosmetics_cli/cli'
+require_relative './cosmetics_cli/cosmetics'
 
 
-puts "environment file"
+
+
