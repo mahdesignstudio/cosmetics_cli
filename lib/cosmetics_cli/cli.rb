d002a1bed21d79d9e00b8603e9e6c0eed7ae4c1f
mahdesignstudio <mahdesignstudio@gmail.com>
2020-03-23 20:02:05 -0400

    stub out project controller file

--- lib/cosmetics_cli/cli.rb
+++ lib/cosmetics_cli/cli.rb
@@ -1,8 +1,27 @@
 class CosmeticsCli::CLI 
     
     def call 
-        puts "Im in the CLI"
+        puts "Welcome user"
+        puts "To see Makeup Products, enter 'makeup'"
+        puts "To exit the program, enter 'goodbye'"
+        menu 
     end 
+
+    def menu 
+        input = gets.strip.downcase
+        
+        if input == "makeup"
+            puts "Let's get started"
+            
+        elsif input == "goodbye"
+            puts "remove makeup"
+            
+        else 
+            puts "Invalid entry, please try again"
+            menu 
+        end
+    end 
+
 
 
 end
