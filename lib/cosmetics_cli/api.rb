0dd5ffe7353280feb45fc535747fbc6c3ea580b2
mahdesignstudio <mahdesignstudio@gmail.com>
2020-03-26 20:06:25 -0400

    define get_data method to add httparty gem

--- lib/cosmetics_cli/api.rb
+++ lib/cosmetics_cli/api.rb
@@ -1,3 +1,11 @@
 class API
     
+    def self.get_data
+        url = 'http://makeup-api.herokuapp.com/api/v1/products.json'
+        response = HTTParty.get(url)
+        response.parsed_response
+
+
+
+
 end 
